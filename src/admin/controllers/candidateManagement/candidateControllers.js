e43bc6cd140a89a93922dc2bc110d177cd9b80d7
Adityakumar92 <aditya.kumar.che21@itbhu.ac.in>
2025-09-10 13:21:31 +0530

    delete button enbale

--- src/admin/controllers/candidateManagement/candidateControllers.js
+++ src/admin/controllers/candidateManagement/candidateControllers.js
@@ -64,6 +64,7 @@ const getCandidateList = async (req, res) => {
     } else {
       sort.createdAt = -1;
     }
+    query.status = { $ne: 3 }; // Exclude deleted candidates
 
     // 📊 Fetch candidates with pagination
     const candidates = await Candidate.find(query)
@@ -86,7 +87,31 @@ const getCandidateList = async (req, res) => {
   }
 };
 
+const deleteCandidate = async(req, res)=>{
+    console.log('params', req.params);
+    try{
+        const candidateId = req.params.id;
+        console.log('candidateId', candidateId);
+        if(!candidateId){
+            return res.status(400).json({success: false, message: "candidateIds parameter is required"});
+        }
+        const isCandidate= await Candidate.findOne({_id:candidateId});
+        if(!isCandidate){
+            return res.status(404).json({success: false, message: "Candidate not found"});
+        }
+        if(isCandidate.status === 3){
+            return res.status(400).json({success: false, message: "Candidate already deleted"});
+        }
+        await Candidate.updateOne({_id:candidateId}, {status: 3});
+        res.status(200).json({success: true, message: "Candidate deleted successfully"});
+    }catch(err){
+        console.error("Error deleting candidates:", err);
+        res.status(500).json({ error: "Something went wrong" });
+    }
+};
+
 module.exports = {
     candidateUser,
-    getCandidateList
+    getCandidateList,
+    deleteCandidate
 }
--- src/admin/routes/candidateManagement/candidateRoutes.js
+++ src/admin/routes/candidateManagement/candidateRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 
-const {candidateUser, getCandidateList} = require('../../controllers/candidateManagement/candidateControllers');
+const {candidateUser, getCandidateList, deleteCandidate} = require('../../controllers/candidateManagement/candidateControllers');
 
 router.get('/candidatesManagement', candidateUser);
 router.post('/getCandidate', getCandidateList);
-
+router.patch('/deleteCandidate/:id', deleteCandidate);
 
 module.exports = router;
